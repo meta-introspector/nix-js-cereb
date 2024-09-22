4e9329da0a067eb37d00746400ee6db8149a8882
tacogips <me@tacogips.me>
2024-09-22 10:41:01 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Basic flake for Astro.js and Bun.js project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+
+          shellHook = '''';
+        };
+      }
+    );
+}
